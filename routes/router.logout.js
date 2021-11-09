ba8aa2d66b2114d5fcd04ee7c9d77d3af6e7aceb
Vladimir <300wrongaway@mail.ru>
2021-11-09 14:46:35 +0300

    All done

---
+++ routes/router.logout.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+router.route("/").get((req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.clearCookie("user_sid");
+
+    return res.redirect("/");
+  });
+});
+
+module.exports = router;
